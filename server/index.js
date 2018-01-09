fe2039573a7097c441e19ac3f99a57dc8f882721
bwilm <32461222+bwilm@users.noreply.github.com>
2018-01-08 22:53:33 -0500

    first draft

---
+++ server/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var api_1 = require("./procedures/api");
+var express = require("express");
+var path = require("path");
+var routing = require('./middleware/routing.mw.js');
+var clientPath = path.join(__dirname, '../client');
+var dataPath = path.join(__dirname, 'data.json');
+var app = express();
+app.use(express.static(clientPath));
+app.use('/api', api_1.default);
+app.get('*', routing.stateRouting);
+console.log('listening');
+app.listen(process.env.PORT || 3000);
